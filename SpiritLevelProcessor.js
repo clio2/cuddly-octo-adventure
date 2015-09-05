fc719c88e9ccf0385aeb43a1308b169db8b13549
clio2 <clio2@student.monash.edu>
2015-09-05 23:11:30 +1000

    Update SpiritLevelProcessor.js

--- SpiritLevelProcessor.js
+++ SpiritLevelProcessor.js
@@ -185,7 +185,7 @@ function SpiritLevelProcessor()
         // Input: x,y,z
         //      These values should be the filtered values after the Moving Average for
         //      each of the axes respectively
-		    var target = document.getElementById("message-area");
+		          var target = document.getElementById("message-area");
 
             var angleZ;
          
@@ -194,7 +194,7 @@ function SpiritLevelProcessor()
             var x2 = Math.pow(x,2);
             var y2 = Math.pow(y,2);
             var z2 = Math.pow(z,2);
-            var Fg = Math.sqrt(x2 + y2 + z2)
+            Fg = Math.sqrt(x2 + y2 + z2)
             
     
             angleZ = (Math.acos(z/Fg) * 180) / Math.PI;
